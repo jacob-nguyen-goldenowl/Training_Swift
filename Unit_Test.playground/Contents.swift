622f697a2ad53acf59bd01297df622b836e579c2
Toan Huu Nguyen <jacob.nguyen.goldenowl@gmail.com>
2022-10-28 12:32:29 +0700

    done unit_test_1

--- Unit_Test.playground/Contents.swift
+++ Unit_Test.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*  UNIT TEST 1 */
+/* 
+ Input: Int number
+ Output: List of divisors
+ 
+ Check: 
+ //1
+ Input: 0
+ Output: nil
+ //2
+ Input: 1
+ Output: [1]
+ */
+
+func printListDivisor(number: Int) -> [Int]? {
+    
+    var list: [Int] = []
+    
+    if number < 1 {
+        return nil
+    }
+    
+    for i in 1 ... number {
+        if number % i == 0 {
+            list.append(i)
+        }
+    }
+    return list
+}
+
+//printListDivisor(number: 26)
+//printListDivisor(number: 1)
+//printListDivisor(number: 0)
