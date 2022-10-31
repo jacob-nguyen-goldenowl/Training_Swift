635a2635e11e2a01941b0db9ca63874281d94b6a
Toan Huu Nguyen <huutoan2603@gmail.com>
2022-10-31 14:43:28 +0700

    inital prj

---
+++ View&Layout/View&Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  View&Layout
+//
+//  Created by Nguyễn Hữu Toàn on 31/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
