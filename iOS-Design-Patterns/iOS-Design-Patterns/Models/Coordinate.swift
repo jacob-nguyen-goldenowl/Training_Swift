801f0751ee59cdc208068122fcb8aef7c7a7705d
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-11-21 14:00:47 +0700

    + New: Create model weather

---
+++ iOS-Design-Patterns/iOS-Design-Patterns/Models/Coordinate.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinate.swift
+//  iOS-Design-Patterns
+//
+//  Created by Nguyễn Hữu Toàn on 21/11/2022.
+//
+
+import Foundation
+
+struct Coordinate: Codable {
+    let lon: Double
+    let lat: Double
+}
